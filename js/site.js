a8a13717557be1b008520e5c931e5a5bab106664
mpmicheal <111928941+mpmicheal@users.noreply.github.com>
2025-03-08 00:40:14 -0600

    add submit javascript

--- js/site.js
+++ js/site.js
@@ -1,10 +1,12 @@
-$("#button").on("click", function (event) {
-  event.preventDefault();
-  let userName = $("#name").val().trim();
-  if (userName) {
-    alert("Thanks for subscribing, " + userName + "!");
-  } else {
-    alert("Please enter your name before subscribing.");
-  }
-});
+$(document).ready(function () {
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    let userName = $("#name").val().trim();
+    if (userName) {
+      alert("Thanks for subscribing, " + userName + "!");
+    } else {
+      alert("Please enter your name before subscribing.");
+    }
+    $(this).trigger("reset");
+  });
 });
