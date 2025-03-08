14446d9ec145658213e1cf6cb776da3805dd75fd
mpmicheal <111928941+mpmicheal@users.noreply.github.com>
2025-03-08 00:17:34 -0600

    remove checkbox java

--- js/site.js
+++ js/site.js
@@ -1,19 +1,4 @@
-jQuery(function ($) {
-  $('html').removeClass('nojs').addClass('hasjs');
-  
-  $(".checkbox-container").on("click", function () {
-    let checkbox = $("#subscribe");
-    checkbox.prop("checked", !checkbox.prop("checked"));
-
-    if (checkbox.prop("checked")) {
-      $(".checkbox-container label").append(" <span class='checkmark'>&#10004;</span>");
-    } else {
-      $(".checkmark").remove();
-    }
-  });
-});
-
- $("#button").on("click", function (event) {
+$("#button").on("click", function (event) {
   event.preventDefault();
   let userName = $("#name").val().trim();
   if (userName) {
